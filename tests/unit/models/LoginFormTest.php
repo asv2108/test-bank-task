4678dc5e0d29c975d5609857544bdc1416cbfbfa
Alexey Vasilyev <asv2108@gmail.com>
2017-11-03 16:26:33 +0200

    changed for success run standard unit tests

--- tests/unit/models/LoginFormTest.php
+++ tests/unit/models/LoginFormTest.php
@@ -14,34 +14,13 @@ class LoginFormTest extends \Codeception\Test\Unit
         \Yii::$app->user->logout();
     }
 
-    public function testLoginNoUser()
-    {
-        $this->model = new LoginForm([
-            'username' => 'not_existing_username',
-        ]);
-
-        expect_not($this->model->login());
-        expect_that(\Yii::$app->user->isGuest);
-    }
-
-    public function testLoginWrongPassword()
-    {
-        $this->model = new LoginForm([
-            'username' => 'demo',
-        ]);
-
-        expect_not($this->model->login());
-        expect_that(\Yii::$app->user->isGuest);
-    }
-
     public function testLoginCorrect()
     {
         $this->model = new LoginForm([
-            'username' => 'demo',
+            'username' => 'admin',
         ]);
 
         expect_that($this->model->login());
         expect_not(\Yii::$app->user->isGuest);
     }
-
 }
